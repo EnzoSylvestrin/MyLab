a32da2e642ac25d111714b87203e8d3805ddf72d
EnzoSylvestrin <enzospavani@gmail.com>
2023-02-27 22:43:19 -0300

    Configurações iniciais e adicionando head padrão

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>MyLab</title>
+        <meta name="description" content="Site feito para aplicar conceitos acadêmicos por meio da programação" />
+        <meta name="author" content="Enzo Sylvestrin - enzospavani@gmail.com" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
